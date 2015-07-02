1b9ae61d9c46df1e16a8bdac7a48855b331bba07
Matthew Kaufer <mjkaufer@gmail.com>
2015-07-02 16:10:12 -0400

    More features, added Todo

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # CodeCompare
 Tool to compare two code snippets with an iterating variable n
+
+## Usage
+
+Type your starting value for `n`, your ending value for `n`, and your code snippets in the appropriate fields. You can reference `n` in your snippets, and it will be interpreted as the current iteration.
+
+The code which is executing behind the scenes kind of looks like this
+
+```JavaScript
+
+	for(var n = startN; n <= endN; n++){
+		function1();
+		function2();
+	}
+
+```
+
+Again, both functions can access the variable `n` - they're computed using `eval`.
+
+
+## Todo
+
+* Support for async functions
+* Better way to not block UI thread
+* Color choosing
+* Integrate with Brython/Skulpt to support Python
--- processing.js
+++ processing.js
@@ -1,74 +1,81 @@
-var n = 100;
+function setDisabled(){
+	var button = $('submit')
+	button.disabled = "true"
+	button.className = baseButtonClassName + " btn-danger"
+	$('wait').style.display = ""
+	console.log("Disabled")
+}
 
-var data = [{
-	"sale": "0",
-	"year": "0"
-}, {
-	"sale": "10",
-	"year": "01"
-}, {
-	"sale": "20",
-	"year": "02"
-}, {
-	"sale": "30",
-	"year": "03"
-}, {
-	"sale": "40",
-	"year": "03"
-}, {
-	"sale": "50",
-	"year": "10"
-}];
-
-var data2 = [{
-	"sale": "10",
-	"year": "0"
-}, {
-	"sale": "20",
-	"year": "01"
-}, {
-	"sale": "30",
-	"year": "02"
-}, {
-	"sale": "30",
-	"year": "03"
-}, {
-	"sale": "40",
-	"year": "03"
-}, {
-	"sale": "50",
-	"year": "10"
-}];
+function setEnabled(){
+	var button = $('submit')
+	button.disabled = ""
+	button.className = baseButtonClassName + " btn-success"
+	$('wait').style.display = "none"
+	console.log("Enabled")
+}
 
 function clear(){
 	d3.selectAll("svg > *").remove();
 }
 
-function newData(points, maxY){
-	return d3.range(points).map(function(item,index){
-		return { x:index, y:Math.random() * (maxY || 10) };
-	});
+function time(){
+	if(performance && performance.now())
+		return performance.now()
+	return Date.now()
 }
 
+function dataFromFunctionString(startIter, endIter, funcString){
 
-function newChart(maxX, maxY){
+	// iterate through all functions, collect list of runtimes
+	// todo - compatibility for async code
+	console.log("")//message to 'warm' up console
+	var __runTimes = []
+	var __maxTime = -1
+	for(var __i = startIter; __i <= endIter; __i++){
+		var n = __i;
+		var __start = performance.now()
+		eval(funcString)
+		var __end = performance.now()
+		var d = __end - __start
+		if(d > __maxTime || __maxTime == -1)
+			__maxTime = d
+		__runTimes.push({x: __i, y: d})
+	}
+	return {data: __runTimes, maxTime: __maxTime}
+
+}
+var vis = d3.select("#visualisation"),
+	WIDTH = 1000,
+	HEIGHT = 500,
+	MARGINS = {
+		top: 50,
+		right: 50,
+		bottom: 50,
+		left: 100
+	}
+
+function newChart(startIter, endIter, func1, func2, col0, col1){
+
 	clear()
-	var vis = d3.select("#visualisation"),
-		WIDTH = 1000,
-		HEIGHT = 500,
-		MARGINS = {
-			top: 50,
-			right: 50,
-			bottom: 50,
-			left: 100
-		},
-		xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([0,maxX]),
+
+	col0 = col0 || "blue"
+	col1 = col1 || "green"
+
+	var funcData1 = dataFromFunctionString(startIter, endIter, func1)
+	var funcData2 = dataFromFunctionString(startIter, endIter, func2)
+
+	var maxY = Math.max(funcData2.maxTime, funcData1.maxTime)
+
+
+	var xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([startIter,endIter]),
 		yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([0,maxY]),
 		xAxis = d3.svg.axis()
-			.scale(xScale),
+			.scale(xScale)
+			.tickFormat(d3.format("d")),
 		yAxis = d3.svg.axis()
 			.scale(yScale)
-			.orient("left");
+			.orient("left")
+			.tickFormat(d3.format("d"));
 
 	vis.append("svg:g")
 		.attr("class","axis")
@@ -88,7 +95,7 @@ function newChart(maxX, maxY){
 	vis.append("text")
 		.attr("transform", "translate(" + MARGINS.left / 2 + "," + HEIGHT / 2 + ") rotate(-90)")
 		.style("text-anchor", "middle")
-		.text("Time")
+		.text("Time (ms)")
 
 	var lineGen = d3.svg.line()
 		.x(function(d) {
@@ -99,20 +106,53 @@ function newChart(maxX, maxY){
 		})
 		.interpolate("basis");
 
+	document.getElementById('code0').parentNode.children[0].style.color = col0
+	document.getElementById('code1').parentNode.children[0].style.color = col1
 
 	vis.append('svg:path')
-		.attr('d', lineGen(newData(maxX, maxY)))
-		.attr('stroke', 'blue')
+		.attr('d', lineGen(funcData1.data))
+		.attr('stroke', col0)
 		.attr('stroke-width', 2)
 		.attr('fill', 'none');
 
 	vis.append('svg:path')
-		.attr('d', lineGen(newData(maxX, maxY)))
-		.attr('stroke', 'green')
+		.attr('d', lineGen(funcData2.data))
+		.attr('stroke', col1)
 		.attr('stroke-width', 2)
 		.attr('fill', 'none');
+	setEnabled()
 
 	return vis
 }
 
-newChart(50, 50)
+
+function $(id){
+	return document.getElementById(id)
+}
+
+function generateChart(){
+
+	var startN = parseInt($('n0').value)
+	var endN = parseInt($('n1').value)
+
+	var firstCode = $('code0').value
+	var secondCode = $('code1').value
+
+	setDisabled()
+
+	setTimeout(function(){
+		newChart(startN, endN, firstCode, secondCode)
+	}, 50)
+	
+
+}
+
+document.getElementById('submit').onclick = function(e){
+	var v = document.getElementById('visualisation')
+	v.style.height = HEIGHT
+	v.style.width = WIDTH
+	e.preventDefault()
+	generateChart()
+}
+
+var baseButtonClassName = "btn btn-block";
